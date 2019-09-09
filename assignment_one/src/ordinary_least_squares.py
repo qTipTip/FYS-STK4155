d47dffc60dd06e6cee2d969c0bbb4492bcc9b4a0
Ivar Stangeby <istangeby@gmail.com>
2019-09-09 13:11:00 +0200

    Added pseudo_inverse based ordinary least squares.

--- assignment_one/src/ordinary_least_squares.py
+++ assignment_one/src/ordinary_least_squares.py
@@ -18,9 +18,50 @@ def design_matrix_from_parameters(x, y, polynomial_degree=2, intercept=True):
     return poly.fit_transform(X)
 
 
+def ordinary_least_squares(X, y, pseudo_inv=True):
+    """
+    Solves the ordinary least squares problem by solving the normal equations X^T y =  X^T X b for b.
+    :param pseudo_inv: uses an svd based approach for the inverse
+    :param X: design matrix
+    :param y: ground truth / response variable
+    :return: the parameters b minimizing the mean squared error.
+    """
+
+    dot = X.T.dot(X)
+
+    if pseudo_inv:
+        dot_inv = svd_inv(dot)
+    else:
+        dot_inv = np.linalg.inv(dot)
+
+    b = dot_inv.dot(X.T).dot(y)
+    return b
+
+
+def svd_inv(A):
+    """
+    Returns the inverse of the matrix A based on a singular value decomposition of A.
+
+    :param A: matrix to invert
+    :return: inv(A)
+    """
+
+    u, sigma, v_transposed = np.linalg.svd(A)
+    m = u.shape[0]
+    n = v_transposed.shape[0]
+
+    D = np.zeros((m, n))
+    for i in range(n):
+        D[i, i] = sigma[i]
+
+    return v_transposed.T @ (np.linalg.pinv(D) @ u.T)
+
+
 if __name__ == '__main__':
     x = np.linspace(0, 1, 3)
-    y = np.linspace(0, 1, 3)
+    y = np.linspace(0, 1, 3) + np.random.normal(0, 1, 3)
+    y_ = np.linspace(0, 1, 3)
 
-    X = design_matrix_from_parameters(x, y)
-    X_ = design_matrix_from_parameters(x, y, intercept=False)
+    X = design_matrix_from_parameters(x, y, polynomial_degree=1)
+    X_ = svd_inv(X)
+    b = ordinary_least_squares(X, y_, pseudo_inv=False)
