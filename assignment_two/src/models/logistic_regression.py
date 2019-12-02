919d2b466d3caa415fef14ffa9256cd06046d596
ivar <istangeby@gmail.com>
2019-12-02 15:45:17 +0100

    Done with CreditCard prediction using sklearn logistic regression.

--- assignment_two/src/models/logistic_regression.py
+++ assignment_two/src/models/logistic_regression.py
@@ -1,6 +1,8 @@
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV, SGDClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
 
 from assignment_two.src.utils.data_loaders import CreditCardData
 
@@ -8,10 +10,32 @@ credit_card_data = CreditCardData()
 X = credit_card_data.X.numpy()
 y = credit_card_data.y.numpy()
 
-print()
-clf = LogisticRegression(penalty='l2', fit_intercept=False, solver='lbfgs')
+model_accuracy = []
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+l1_params = [
+    {
+        'penalty': ['l1'],
+        'C': np.logspace(-4, 4, 40),
+        'solver': ['liblinear']
+    },
+]
+l2_params = [
+    {
+        'penalty': ['l2'],
+        'C': np.logspace(-4, 4, 40),
+        'solver': ['liblinear']
+    }
+]
 
-clf.fit(X_train, y_train)
-print(f'scikit-learn logistic regression accuracy = {clf.score(X_test, y_test):.03f}')
+logspace = np.logspace(0, -3, 100)
+clf_l1 = GridSearchCV(LogisticRegression(), l1_params, cv=5, verbose=True, n_jobs=-1)
+clf_l2 = GridSearchCV(LogisticRegression(), l2_params, cv=5, verbose=True, n_jobs=-1)
+
+clf_l1.fit(X, y)
+clf_l2.fit(X, y)
+
+plt.semilogx(l1_params[0]['C'], clf_l1.cv_results_['mean_test_score'], label='l1')
+plt.semilogx(l2_params[0]['C'], clf_l2.cv_results_['mean_test_score'], label='l2')
+
+plt.legend()
+plt.show()
